3ceec646c10889d2d2f9e57cb18d123b306c1905
Vinicius Mussak <vinicius.mussak@outlook.com>
2019-07-07 15:52:02 -0300

    Add project files.

---
+++ MesPorExtenso/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace MesPorExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExibirMesPorExtenso(new DateTime(2019, 7, 7));
+
+            Console.ReadKey();
+        }
+
+        public static void ExibirMesPorExtenso(DateTime data)
+        {
+            var idioma = CultureInfo.CreateSpecificCulture("pt-br");
+
+            Console.WriteLine(data.ToString("MMMM", idioma));
+            Console.WriteLine("---");
+            Console.WriteLine(data.ToString("MMM", idioma));
+            Console.WriteLine("---");
+            Console.WriteLine(data.ToString("MM", idioma));
+            Console.WriteLine("---");
+            Console.WriteLine(data.ToString("M", idioma));
+        }
+    }
+}
